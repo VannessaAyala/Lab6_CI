09f9b3761a5d36cc9668a634c31a3776cabeabf4
VannessaAyala <vane992059@gmail.com>
2026-07-17 15:57:12 -0500

    Proyecto base de CI con SpringBoot y Pruebas Unitarias

---
+++ src/main/java/ec/edu/espe/lab6_ci/service/WalletService.java
@@ -0,0 +1,64 @@
+package ec.edu.espe.lab6_ci.service;
+
+import ec.edu.espe.lab6_ci.dto.WalletResponse;
+import ec.edu.espe.lab6_ci.model.Wallet;
+import ec.edu.espe.lab6_ci.repository.WalletRepository;
+
+import java.util.Optional;
+
+public class WalletService {
+    private final WalletRepository walletRepository;
+    private final RiskClient riskClient;
+
+    public WalletService(WalletRepository walletRepository, RiskClient riskClient) {
+        this.walletRepository = walletRepository;
+        this.riskClient = riskClient;
+    }
+
+    //Crear billetera
+    public WalletResponse createWallet(String ownerEmail, double initialBalance) {
+        //Validaciones
+        if(ownerEmail==null || !ownerEmail.contains("@")){
+            throw new IllegalArgumentException("Invalid email");
+        }
+
+        if(initialBalance<0){
+            throw new IllegalArgumentException("Invalid balance must be >= 0");
+        }
+
+        //Regla de negocio: usuario bloqueado
+        if(riskClient.isBlocked(ownerEmail)){
+            throw new IllegalStateException("User blocked");
+        }
+
+        //Regla de negocio: no duplicar billetera por email
+        if(walletRepository.existByOwnerEmail(ownerEmail)){
+            throw new IllegalStateException("Wallet already exists");
+        }
+
+        Wallet wallet = new Wallet(ownerEmail, initialBalance);
+        Wallet saved = walletRepository.save(wallet);
+
+        return new WalletResponse(saved.getId(), saved.getBalance());
+    }
+
+    //Depositar dinero
+    public double deposit(String walletId, double amount) {
+        if(amount <= 0){
+            throw new IllegalArgumentException("Deposit amount must be > 0");
+        }
+
+        Optional<Wallet> found = walletRepository.findById(walletId);
+        if(found.isEmpty()){
+            throw new IllegalStateException("Wallet not found");
+        }
+
+        Wallet wallet = found.get();
+        wallet.deposit(amount);
+
+        //Persistencia a la BDD
+        walletRepository.save(wallet);
+
+        return wallet.getBalance();
+    }
+}
